30cae43ea7613ab75bf55076cd859efc4cd96adc
Chen_xiaojun <1032455194@qq.com>
2016-04-04 22:32:13 +0800

    第一次提交，正在修改两个图标

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by SuMux on 2016/4/3 0003.
+ */
+
+/*window.onscroll = function () {
+    var t = document.documentElement.scrollTop || document.body.scrollTop;
+    var headerBarBack = document.getElementById("headerBarBack");
+    var contentBack = document.getElementById("contentBack");
+
+    if( t >= 1 ) {
+        headerBarBack.style.height = "80px";
+        contentBack.style.paddingTop = "80px"
+    }
+    else{
+        headerBarBack.style.height = "300px";
+        contentBack.style.paddingTop = "300px";
+    }
+};*/
