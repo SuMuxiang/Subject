14e331ae9110075903f81b69b0bce4dbafedc739
Chen_xiaojun <1032455194@qq.com>
2016-04-05 17:15:59 +0800

    第二次提交，头部原型基本完成。

--- js/index.js
+++ js/index.js
@@ -1,18 +1,77 @@
 /**
  * Created by SuMux on 2016/4/3 0003.
  */
-
-/*window.onscroll = function () {
-    var t = document.documentElement.scrollTop || document.body.scrollTop;
-    var headerBarBack = document.getElementById("headerBarBack");
-    var contentBack = document.getElementById("contentBack");
-
-    if( t >= 1 ) {
-        headerBarBack.style.height = "80px";
-        contentBack.style.paddingTop = "80px"
+function id( x ) {
+    if( typeof x == "string" ) {
+        return document.getElementById(x);
     }
-    else{
-        headerBarBack.style.height = "300px";
-        contentBack.style.paddingTop = "300px";
+    return x;
+}
+
+function hasClass(obj, cls) {
+    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(obj, cls) {
+    if (!this.hasClass(obj, cls)) obj.className += " " + cls;
+}
+
+function removeClass(obj, cls) {
+    if (hasClass(obj, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        obj.className = obj.className.replace(reg, ' ');
     }
-};*/
+}
+
+function toggleClass(obj,cls){
+    if(hasClass(obj,cls)){
+        removeClass(obj, cls);
+    }else{
+        addClass(obj, cls);
+    }
+}
+
+window.onload = function() {
+    var headerBarBack = id('headerBarBack');
+    var arrowBack = id('arrowBack');
+    var welcome = id('welcome');
+    var welcomeDescription = id('welcomeDescription');
+
+    arrowBack.onclick = function() {
+        welcome.innerHTML = 'Welcome';
+        welcomeDescription.innerHTML = 'Welcome to here to visit our website.';
+        headerBarBack.style.backgroundColor = 'cornflowerblue';
+    };
+
+    var headerMenuLi = document.getElementsByClassName('headerMenuLi');
+
+    for(var i = 0; i < headerMenuLi.length; i++) {
+        headerMenuLi[i].onclick = (function(i) {
+            return function() {
+                welcome.innerHTML = headerMenuLi[i].innerHTML;
+                welcomeDescription.innerHTML = 'This is the ' + headerMenuLi[i].innerHTML +  ' page.';
+                headerBarBack.style.backgroundColor = 'green';
+            }
+        })(i); // 闭包函数
+    }
+
+    var searchInput = id('searchInput');
+
+    id('searchInputLabel').onclick = function() {
+        if (hasClass(searchInput, 'searchInputHide')) {
+            removeClass(searchInput, 'searchInputHide');
+            addClass(searchInput, 'searchInputShow');
+            var searchInterval = setInterval(function(){
+                if(searchInput.value === ''){
+                    removeClass(searchInput, 'searchInputShow');
+                    addClass(searchInput, 'searchInputHide');
+                    window.clearInterval(searchInterval);
+                }
+            },10000);
+        }
+        /*else {
+            removeClass(searchInput, 'searchInputShow');
+            addClass(searchInput, 'searchInputHide');
+        }*/
+    }
+};
